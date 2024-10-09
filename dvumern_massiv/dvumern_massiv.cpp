bf973a737a7337867e5ab72eb6660afe24f472d9
leysa <leysa@>
2024-10-09 20:17:23 +0300

    Добавьте файлы проекта.

---
+++ dvumern_massiv/dvumern_massiv.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+#include<windows.h>
+using namespace std;
+int main()
+{
+	int arr[3][3] = { {-1, 2, 8}, {-6,5,9}, {11,4,6} };
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++) 
+		{
+			cout << arr[i][j] <<"\t";
+		}
+		cout << endl;
+	}
+	int min_ = arr[0][0];
+	int max_ = arr[0][0];
+	for (size_t i = 0; i < 3; i++)
+	{
+		for (size_t j = 0; j < 3; j++)
+		{
+			if (min_ > arr[i][j])
+			{
+				min_ = arr[i][j];
+			}
+			else
+				if(max_ < arr[i][j])
+			{
+				max_ = arr[i][j] ;
+			}
+		}
+	}
+	cout << "min = " << min_ << endl;
+	cout << "max = " << max_ << endl;
+}
